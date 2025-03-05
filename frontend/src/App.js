486a9fd6092c517056c6466a0f027044b891fc55
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-05 21:41:02 +0200

    add AuthenticationPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,6 +55,10 @@ const router = createBrowserRouter([
         ],
       },
       {
+        path: 'auth',
+        element: <AuthenticationPage />,
+      },
+      {
         path: 'newsletter',
         element: <NewsletterPage />,
         action: newsletterAction,
