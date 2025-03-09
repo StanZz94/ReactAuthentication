640edef97d88670edeb95bc6834b6e1a4e8bafa3
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-09 22:01:23 +0200

    create CheckAuthLoader util

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
     const token = localStorage.getItem('token');
 
@@ -5,6 +7,14 @@ export function getAuthToken() {
 }
 
 export function tokenLoader() {
-    
+
     return getAuthToken();
+}
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+
+    if(!token) {
+        return redirect('/auth');
+    }
 }
